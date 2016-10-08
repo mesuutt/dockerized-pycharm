fbd99b4feb53af72ea959c24450ec0d8718d7e7a
Mesut Tasci <slackermesut@gmail.com>
2016-10-08 23:57:22 +0300

    Remove unnecessary spaces

--- pycharm.sh
+++ pycharm.sh
@@ -11,13 +11,13 @@ mkdir -p $DATA_DIR/pycharm
 xhost +
 
 docker run -d \
-	-v /etc/localtime:/etc/localtime:ro \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-e DISPLAY=unix$DISPLAY \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
     -v $DATA_DIR/java:/home/developer/.java \
     -v $DATA_DIR/pycharm:/home/developer/.PyCharm2016.2 \
     -v $PROJECTS_DIR:/home/developer/projects \
-	--name pycharm \
-	mesuutt/pycharm:professional
+    --name pycharm \
+    mesuutt/pycharm:professional
 
 xhost -
