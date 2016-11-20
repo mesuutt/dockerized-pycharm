407c5cc0e1d19a2b241b9fa3cbb485b5d30e440a
Mesut Tasci <slackermesut@gmail.com>
2016-11-20 13:49:32 +0200

    Access to X socket with magic cookies from container for security
    For more info: http://wiki.ros.org/docker/Tutorials/GUI

--- pycharm.sh
+++ pycharm.sh
@@ -8,16 +8,20 @@ DATA_DIR=`pwd`/.pycharm
 mkdir -p $DATA_DIR/java
 mkdir -p $DATA_DIR/pycharm
 
-xhost +
+XAUTH=/tmp/.dockerized-pycharm.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
 
 docker run -d \
     -v /etc/localtime:/etc/localtime:ro \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
+    -v $XAUTH:$XAUTH \
+    -e XAUTHORITY=$XAUTH \
     -v $DATA_DIR/java:/home/developer/.java \
     -v $DATA_DIR/pycharm:/home/developer/.PyCharm2016.2 \
     -v $PROJECTS_DIR:/home/developer/projects \
     --name pycharm \
     mesuutt/pycharm:professional
 
-xhost -
